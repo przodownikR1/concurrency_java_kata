258f70f779c9e2f683c8bb555b91346760df9186
Slawomir Borowiec <przodownik@tlen.pl>
2015-06-09 14:41:07 +0200

    customAsyncException handler  added

--- src/main/java/pl/java/scalatech/config/AppConfig.java
+++ src/main/java/pl/java/scalatech/config/AppConfig.java
@@ -1,21 +1,43 @@
 package pl.java.scalatech.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import pl.java.scalatech.concurrent.ReadWriteResource;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
-public class AppConfig {
+public class AppConfig implements SchedulingConfigurer, AsyncConfigurer {
     @Bean
     public ReadWriteResource readWriteResource() {
         return new ReadWriteResource();
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(10);
     }
 
     @Bean
@@ -25,6 +47,25 @@ public class AppConfig {
         threadPoolTaskExecutor.setCorePoolSize(2);
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
+    }
+
+    private ThreadFactory threadFactory() {
+        return new ThreadFactoryBuilder().setNameFormat("my-pool-id").build();
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public ExecutorService executors() {
+        return Executors.newFixedThreadPool(5, threadFactory());
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new CustomAsyncExceptionHandler();
     }
 
     // SimpleAsyncTaskExecutor - > start a new theard for each invocation. Supports a concurrency limit,
---
+++ src/main/java/pl/java/scalatech/config/CustomAsyncExceptionHandler.java
@@ -0,0 +1,20 @@
+package pl.java.scalatech.config;
+
+import java.lang.reflect.Method;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+
+@Slf4j
+public class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+    @Override
+    public void handleUncaughtException(final Throwable throwable, final Method method, final Object... obj) {
+        log.error("Exception message - " + throwable.getMessage());
+        log.error("Method name - " + method.getName());
+        for (final Object param : obj) {
+            log.error("Param - " + param);
+        }
+    }
+
+}
